3c9e2b9515efa44962665783b6c32d732cedab4a
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-09-19 23:22:32 -0400

    Info fetched from api

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let num = Math.floor(Math.random() * 200);
-
-const url = `https://opencritic-api.p.rapidapi.com/game/${num}`;
+let data;
 const options = {
   method: "GET",
+  url: "https://opencritic-api.p.rapidapi.com/game/hall-of-fame/2016",
   headers: {
     "X-RapidAPI-Key": "4b961ab84dmshd7d90f4d1e3c191p187930jsne0c60ebee570",
     "X-RapidAPI-Host": "opencritic-api.p.rapidapi.com",
@@ -10,11 +10,19 @@ const options = {
 };
 const getStuff = async () => {
   try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result.name);
+    const response = await fetch(options.url, options);
+    data = await response.json();
+    getPic();
+    console.log(pic.src);
+    // if (!result.name) ;
   } catch (error) {
     console.error(error);
   }
+};
+const getPic = () => {
+  let pic = document.querySelector("#pic");
+  pic.src = `https:img.opencritic.com/${data[1].images.box.og}`;
+  // // pic.src =
+  //   "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ5DGvQUmxxni4gFOyTZkCVgYYKDKVhhF5SwHcfF7zxSX1s13839su9bSTL_lrVdtLFTZ0G-zyEAuBp0t0";
 };
 getStuff();
