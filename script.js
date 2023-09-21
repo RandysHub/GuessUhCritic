7820e307502295ce63d1883436d8d91a94b51f92
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-09-21 03:05:39 -0400

    Able to check answer

--- script.js
+++ script.js
@@ -12,17 +12,31 @@ const getStuff = async () => {
   try {
     const response = await fetch(options.url, options);
     data = await response.json();
-    getPic();
-    console.log(pic.src);
+    placeData();
+    console.log(data);
     // if (!result.name) ;
   } catch (error) {
     console.error(error);
   }
 };
-const getPic = () => {
+const placeData = () => {
   let pic = document.querySelector("#pic");
   pic.src = `https:img.opencritic.com/${data[1].images.box.og}`;
-  // // pic.src =
-  //   "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ5DGvQUmxxni4gFOyTZkCVgYYKDKVhhF5SwHcfF7zxSX1s13839su9bSTL_lrVdtLFTZ0G-zyEAuBp0t0";
+
+  let title = document.querySelector(".title");
+  title.innerHTML = data[1].name;
+
+  let score = document.querySelector(".score");
+  score.innerHTML = data[1].topCriticScore;
 };
+
+const checkGuess = () => {
+  let guess = document.querySelector(".guess");
+  let btn = document.querySelector("#btn");
+  // console.log(guess.value);
+  if (guess.value == data[1].topCriticScore) {
+    console.log("nice");
+  } else console.log("BRICK");
+};
+
 getStuff();
