fe04138b5f86b751359bccb9018785b3cc399925
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-10-11 00:21:22 -0400

    dark mode basic implementation

--- darkMode.js
+++ darkMode.js
@@ -1,23 +1,23 @@
 let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector("dark-mode-button");
+const darkModeToggle = document.querySelector(".dark-mode-button");
 
 const enableDarkMode = () => {
-  document.body.classList.add("dark-mode");
-  localStorage.setItem("darkMode", true);
+  document.querySelector(".info").classList.add("dark-mode");
+  localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
-  document.body.classList.remove("dark-mode");
-  localStorage.setItem("darkMode", false);
+  document.querySelector(".info").classList.remove("dark-mode");
+  localStorage.setItem("darkMode", "disabled");
 };
 
-// darkModeToggle.addEventListener("click", () => {
-//   // darkMode = localStorage.getItem("darkMode")
-//   if (!darkMode) {
-//     enableDarkMode();
-//     console.log(darkMode);
-//   } else {
-//     disableDarkMode();
-//     console.log(darkMode);
-//   }
-// });
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
+});
